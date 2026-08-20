f79525a658bd4699eb6f358da37f566b41a72725
David Figueroa <dgfigueroa29@gmail.com>
2026-08-20 20:00:09 -0300

    fix: finish build

--- composeApp/src/androidMain/kotlin/com/testscanner/di/AppModule.android.kt
+++ composeApp/src/androidMain/kotlin/com/testscanner/di/AppModule.android.kt
@@ -29,6 +29,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 /**
